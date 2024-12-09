79831928beddf0f851531b950dd222a76f2bfa45
Eric <heimleiter.dasheim@gmail.com>
2024-12-09 12:50:08 +0100

    add template day

---
+++ 2024/Template_Day/solution.py
@@ -0,0 +1,15 @@
+def solution():
+    with open('./Day01/input.txt', 'r') as f:
+        data = f.read().splitlines()
+    
+    return 0
+
+def solution2():
+    with open('./Day01/input.txt', 'r') as f:
+        data = f.read().splitlines()
+     
+    return 0
+
+if __name__ == "__main__":
+    print(solution())
+    print(solution2())
