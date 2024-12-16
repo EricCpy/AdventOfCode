034167ed12751a19a5754adf7bbd1d749242a9c6
Eric <heimleiter.dasheim@gmail.com>
2024-12-16 17:02:24 +0100

    day16

--- 2024/Day16/solution.py
+++ 2024/Day16/solution.py
@@ -1,5 +1,5 @@
 from queue import PriorityQueue
-import heapq
+from collections import deque
 
 def solution():
     with open('./Day16/input.txt', 'r') as f:
@@ -45,10 +45,78 @@ def solution():
     return -1
 
 def solution2():
-    with open('./Day01/input.txt', 'r') as f:
+    with open('./Day16/input.txt', 'r') as f:
         data = f.read().splitlines()
-     
-    return 0
+    
+    dirs = [
+        (-1, 0), (0, 1), (1, 0), (0, -1)  # Up, Right, Down, Left
+    ]
+
+    for x, line in enumerate(data):
+        for y, val in enumerate(line):
+            if val == 'S':
+                start_x, start_y = x, y
+    
+    queue = PriorityQueue()
+    visited = {}
+    
+    queue.put((0, start_x, start_y, 1))
+    visited[(start_x, start_y, 1)] = 0
+    
+    backtrack = {}
+    min_cost = float('inf')
+    end_states = set()
+
+    while not queue.empty():
+        cost, x, y, last_dir_idx = queue.get()
+        
+        if cost > visited.get((x, y, last_dir_idx), float('inf')):
+            continue
+
+        if data[x][y] == 'E':
+            if cost > min_cost:
+                continue
+            min_cost = cost
+            end_states.add((x, y, last_dir_idx))
+
+        for i in [0, 1, -1]:
+            dir_idx = (last_dir_idx + i) % len(dirs)
+            dx, dy = dirs[dir_idx]
+            if dir_idx == last_dir_idx:
+                nx, ny = x + dx, y + dy
+                new_cost = cost + 1
+            elif dir_idx != last_dir_idx:
+                nx, ny = x, y
+                new_cost = cost + 1000
+
+            if nx < 0 or ny < 0 or nx >= len(data) or ny >= len(data[nx]) or data[nx][ny] == '#':
+                continue
+
+            if new_cost <= visited.get((nx, ny, dir_idx), float('inf')):
+                if new_cost < visited.get((nx, ny, dir_idx), float('inf')):
+                    backtrack[(nx, ny, dir_idx)] = set()
+                    visited[(nx, ny, dir_idx)] = new_cost
+                    
+                backtrack[(nx, ny, dir_idx)].add((x, y, last_dir_idx))
+                queue.put((new_cost, nx, ny, dir_idx))
+
+    seen = set()
+    states = deque(end_states)
+
+    while states:
+        key = states.popleft()
+        for last in backtrack.get(key, []):
+            if last in seen: 
+                continue
+            
+            seen.add(last)
+            states.append(last)
+
+    s = set()
+    for x in seen:
+        s.add(x[:2])
+
+    return len(s) + 1
 
 if __name__ == "__main__":
     print(solution())
